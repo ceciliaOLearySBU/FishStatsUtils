3d78db68ebfe92737420f46e8b0a5f1bc661cfda
Jim Thorson <JamesT.esq@gmail.com>
2019-01-21 09:22:41 -0800

    Testing brute-force dependencies install

--- R/zzz.R
+++ R/zzz.R
@@ -8,12 +8,44 @@
     #utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
     utils::install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
   }
+  packageStartupMessage("Installing dependencies while setting repos...")
+  Dep = c(
+    "graphics",
+    "utils",
+    "mapproj",
+    "maptools",
+    "deldir",
+    "PBSmapping",
+    "RANN",
+    "stats",
+    "colorspace",
+    "RandomFields",
+    "RandomFieldsUtils",
+    "shape",
+    "devtools",
+    "mixtools",
+    "sp",
+    "maps",
+    "mapdata",
+    "TMB",
+    "MatrixModels",
+    "rgdal",
+    "abind",
+    "corpcor",
+    "pander",
+    "formatR"
+  )
+  utils::install.packages( pkgs=Dep, repos='http://cran.us.r-project.org' )
   #if( !"TMB" %in% utils::installed.packages()[,1] ){
   #  packageStartupMessage("Installing TMB...")
   #  devtools::install_github("kaskr/adcomp/TMB")
   #}
-  #if( !"TMBhelper" %in% utils::installed.packages()[,1] ){
-  #  packageStartupMessage("Installing package: TMBhelper...")
-  #  devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
-  #}
+  if( !"TMBhelper" %in% utils::installed.packages()[,1] ){
+    packageStartupMessage("Installing package: TMBhelper...")
+    devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+  }
+  if( !"ThorsonUtilities" %in% utils::installed.packages()[,1] ){
+    packageStartupMessage("Installing package: ThorsonUtilities...")
+    devtools::install_github("james-thorson/utilities")
+  }
 }
